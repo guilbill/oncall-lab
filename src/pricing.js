af5040cea253e9a57f93d077aa36bb7cfbf199d2
Guillaume BILLEY <guillaume@marmelab.com>
2026-09-18 12:01:14 +0200

    feat(pricing): annotate applyDiscount arguments

--- src/pricing.js
+++ src/pricing.js
@@ -7,7 +7,7 @@ export function applyVat(amountExVat) {
   return Math.round(amountExVat * (1 + VAT_RATE) * 100) / 100;
 }
 
-export function applyDiscount(amount, percent) {
+export function applyDiscount(amount: number, percent) {
   if (percent < 0 || percent > 100) {
     throw new RangeError('discount must be between 0 and 100');
   }
